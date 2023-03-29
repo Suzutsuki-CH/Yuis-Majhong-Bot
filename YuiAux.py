3d1caf9f5acecfac3b94830176f57eef6a6e0922
Suzutsuki-CH <han_chi0727@outlook.com>
2023-03-29 13:43:36 -0400

    Added More Feaures

---
+++ YuiAux.py
@@ -0,0 +1,70 @@
+from mahjong.hand_calculating.hand import HandCalculator
+from mahjong.tile import TilesConverter
+from mahjong.hand_calculating.hand_config import HandConfig
+from mahjong.meld import Meld
+import discord
+import numpy as np
+import os
+
+# Convert user input to tenhou format
+def TenhouGenerate(s):
+    S = np.array(list(s))
+    sig_index = [0]+[i for i in range(len(S)) if ord(S[i]) in [115, 112, 109, 122]]
+
+    # [man, sou, pin, honor]
+    TenhouArr = [[],[],[],[]]
+    man = []
+    sou = []
+    pin = []
+    zi = []
+
+    for i in range(1,len(sig_index)):
+        if S[sig_index[i]] == 'm':
+            m = S[sig_index[i-1]+1 : sig_index[i]]
+            man += list(m)
+
+        if S[sig_index[i]] == 'p':
+            p = S[sig_index[i-1]+1 : sig_index[i]]
+            pin += list(p)
+
+        if S[sig_index[i]] == 's':
+            s = S[sig_index[i-1]+1 : sig_index[i]]
+            sou += list(s)
+
+        if S[sig_index[i]] == 'z':
+            z = S[sig_index[i-1]+1 : sig_index[i]]
+            zi += list(z)
+
+    man.sort()
+    sou.sort()
+    pin.sort()
+    zi.sort()
+            
+    man = "".join(man)
+    sou = "".join(sou)
+    pin = "".join(pin)
+    zi = "".join(zi)
+
+    return [man, sou, pin, zi]
+        
+def LaTeX_Generate(s):
+    with open('LaTeX/Mahjong.tex','w') as file:
+            file.write(r'\documentclass{standalone}'+'\n')
+            file.write(r'\usepackage{tikz}'+'\n')
+            file.write(r'\usepackage{mahjong}'+'\n\n')
+
+            file.write(r'\begin{document}'+'\n')
+            file.write(r'\begin{tikzpicture}'+'\n')
+            file.write(r'\draw (0,0) node {\mahjong[%s]{%s}};'%('3cm',s)+'\n')
+            file.write(r'\end{tikzpicture}'+'\n')
+            file.write(r'\end{document}')
+
+    os.system('"/Users/suzutsuki-ch/bin/xelatex" -interaction=nonstopmode LaTeX/Mahjong.tex')
+    os.system('convert -density 300 Mahjong.pdf -quality 110 LaTeX/Tiles/temp.png')
+    
+def CleanUp():
+    os.remove('LaTeX/Tiles/temp.png')
+    os.remove('Mahjong.pdf')
+    os.remove('Mahjong.log')
+    os.remove('Mahjong.aux')
+     
--- YuiMahjong.py
+++ YuiMahjong.py
@@ -1,9 +1,18 @@
 import discord
 import os
 import numpy as np
+from YuiAux import *
+
+from mahjong.hand_calculating.hand import HandCalculator
+from mahjong.tile import TilesConverter
+from mahjong.hand_calculating.hand_config import HandConfig
+from mahjong.meld import Meld
+from mahjong.shanten import Shanten
+
 
 
 MAHJONG_ASCII = [n for n in range(48,58)] + [115, 112, 109, 122]
+shanten = Shanten()
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -18,39 +27,57 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    sent = False
     if message.author != client.user:
         print(message.author, message.content)
 
-        if 'oppai' in message.content:
+        if np.random.randint(6) == 0:
+            await message.channel.send(np.random.choice(['<:yuiNotme:1009859812441456670>',
+                                        '<:yuiOVO:1009859221313040435>', 
+                                        '<:yuilovelove:1010149916728885259>',
+                                        '<:yuithumbdown:1009859498346807317>',
+                                        '<:yuiO:1009859418965422150>',
+                                        '<:yuiStink:1009859639929749504>']))
+            print("sent")
+            sent = True
+
+
+        if 'oppai' in message.content and not sent:
             print("sent")
             await message.channel.send('BOOBs')
+            sent = True
 
-        if 'yui' in message.content:
+        if 'yui' in message.content and np.random.randint(2)==1 and not sent:
+            odd = np.random.randint(6)
             print("sent")
-            await message.channel.send('<:lilyO:1009861944150343770>')
+            await message.channel.send(['<:yuiNotme:1009859812441456670>',
+                                        '<:yuiOVO:1009859221313040435>', 
+                                        '<:yuilovelove:1010149916728885259>',
+                                        '<:yuithumbdown:1009859498346807317>',
+                                        '<:yuiO:1009859418965422150>',
+                                        '<:yuiStink:1009859639929749504>'][odd])
+            sent = True
+
+        if message.content.startswith("suan"):
+            t = message.content[5:]
+            if np.all([ord(s) in MAHJONG_ASCII for s in t]):
+                TenhouArr = TenhouGenerate(t)
+                tiles = TilesConverter.string_to_34_array(man=TenhouArr[0], sou=TenhouArr[1], pin=TenhouArr[2], honors=TenhouArr[3])
+                result = shanten.calculate_shanten(tiles)
+                await message.channel.send(f"小唯再摸{result}張牌就可以聽牌了！")
+                LaTeX_Generate(t)
+                await message.channel.send(file = discord.File('LaTeX/Tiles/temp.png'))
+                CleanUp()
+
+
 
         if message.content.startswith('mahjong'):
 
             tiles = message.content[8:]
             if np.all([ord(s) in MAHJONG_ASCII for s in tiles]):
-                with open('LaTeX/Mahjong.tex','w') as file:
-                    file.write(r'\documentclass{standalone}'+'\n')
-                    file.write(r'\usepackage{tikz}'+'\n')
-                    file.write(r'\usepackage{mahjong}'+'\n\n')
-
-                    file.write(r'\begin{document}'+'\n')
-                    file.write(r'\begin{tikzpicture}'+'\n')
-                    file.write(r'\draw (0,0) node {\mahjong[%s]{%s}};'%('3cm',tiles)+'\n')
-                    file.write(r'\end{tikzpicture}'+'\n')
-                    file.write(r'\end{document}')
-
-                os.system('"/Users/suzutsuki-ch/bin/xelatex" -interaction=nonstopmode LaTeX/Mahjong.tex')
-                os.system('convert -density 300 Mahjong.pdf -quality 110 LaTeX/Tiles/temp.png')
+                LaTeX_Generate(tiles)
                 await message.channel.send(file = discord.File('LaTeX/Tiles/temp.png'))
-                os.remove('LaTeX/Tiles/temp.png')
-                os.remove('Mahjong.pdf')
-                os.remove('Mahjong.log')
-                os.remove('Mahjong.aux')
+                CleanUp()
 
     
     # your code to process the message goes here
